d3e77138d00c19e8096499f7b0504a1d59e34c95
wanqiguo <wanqiguo2@gmail.com>
2019-01-07 16:27:06 +0800

    pawn trigger open door

---
+++ Source/RoomEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();	
+	actor = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	FRotator newRotator = FRotator(0.f, openAngle, -0.f);
+	GetOwner()->SetActorRotation(newRotator);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (pressurePlate && pressurePlate->IsOverlappingActor(actor)) {
+		OpenDoor();
+	}
+}
+
---
+++ Source/RoomEscape/PositionReporter.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReporter.h"
+#include "Gameframework/Actor.h"
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+	FString name = GetOwner()->GetName();
+	FString pos = GetOwner()->GetActorLocation().ToString();
+	//FString pos = ("%s", GetActorLocation().ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *name, *pos);
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
